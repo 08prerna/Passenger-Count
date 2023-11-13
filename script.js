ff972086dd9e2545b64afa71431ad0abecde81e5
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2023-11-13 23:23:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var h2Element = document.getElementById('counter');
 var deatils=document.getElementById('history');
 var count=0;
-
+var clr = document.getElementById('clr');
 function increment() {
     count=count+1;
     h2Element.innerHTML=count;
@@ -13,3 +13,10 @@ function save() {
     h2Element.textContent=0;
     count=0;
 }
+
+
+clr.addEventListener(`click`,()=>{
+h2Element.textContent = 0;
+deatils.innerHTML = "Previous entries: 0"
+})
+
